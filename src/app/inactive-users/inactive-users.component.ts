8366eb9f6f921e0b145a9552df1b57681156ee0c
hav0kinho <ruallysonfelype@gmail.com>
2024-01-26 15:22:50 -0300

    Exercicio concluido - Mudança para Services feitas com sucesso

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,24 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
   styleUrls: ['./inactive-users.component.css']
 })
-export class InactiveUsersComponent {
-  @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
+export class InactiveUsersComponent implements OnInit {
+  // @Input() users: string[];
+  // @Output() userSetToActive = new EventEmitter<number>();
+
+  users: string[] = [];
+
+  constructor(private userService: UsersService) { }
+
+  ngOnInit() {
+    this.users = this.userService.inactiveUsers;
+  }
 
   onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+    this.userService.onSetToActive(id);
   }
 }
---
+++ src/app/services/counter.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@angular/core";
+import { UsersService } from "./users.service";
+
+@Injectable()
+export class CounterService {
+
+}
